cc2027ebb75f2a374c773273ccf8d7858da31151
kristof_berge <kristof.berge@granter.com.br>
2020-08-22 18:36:52 -0300

    Part 02

---
+++ Part 02/PunkyBeer/PunkyBeer/App.xaml.cs
@@ -0,0 +1,38 @@
+using Prism;
+using Prism.Ioc;
+using PunkyBeer.ViewModels;
+using PunkyBeer.Views;
+using Xamarin.Essentials.Interfaces;
+using Xamarin.Essentials.Implementation;
+using Xamarin.Forms;
+using Prism.DryIoc;
+using PunkyBeer.Services;
+
+namespace PunkyBeer
+{
+    public partial class App
+    {
+        public App(IPlatformInitializer initializer)
+            : base(initializer)
+        {
+        }
+
+        protected override async void OnInitialized()
+        {
+            InitializeComponent();
+
+            await NavigationService.NavigateAsync("NavigationPage/WelcomePage");
+        }
+
+        protected override void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+            containerRegistry.RegisterSingleton<IAppInfo, AppInfoImplementation>();
+            containerRegistry.RegisterSingleton<ISecureStorage, SecureStorageImplementation>();
+            containerRegistry.RegisterSingleton<IUserService, UserService>();
+
+            containerRegistry.RegisterForNavigation<NavigationPage>();
+            containerRegistry.RegisterForNavigation<MainPage, MainPageViewModel>();
+            containerRegistry.RegisterForNavigation<WelcomePage, WelcomePageViewModel>();
+        }
+    }
+}
